e2e002628f3057046c85cd2860a216db13242968
Amit Singh <amitbannaji296@gmail.com>
2025-05-10 22:27:28 +0530

    change

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -18,6 +18,8 @@ export default function Filter({ course, setCourse }: Props) {
             className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 sm:text-sm appearance-none"
           >
             <option value="">All Courses</option>
+            <option value="English">English</option>
+            <option value="Hindi">Hindi</option>
             <option value="Math">Math</option>
             <option value="Physics">Physics</option>
             <option value="Chemistry">Chemistry</option>
